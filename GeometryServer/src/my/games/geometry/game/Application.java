ff1e85030e50ced534a3378d1581340df0a1b648
elldissin <elldissin@gmail.com>
2016-09-12 18:23:19 +0300

    Changed arrays to List<>

--- GeometryServer/src/my/games/geometry/game/Application.java
+++ GeometryServer/src/my/games/geometry/game/Application.java
@@ -5,9 +5,10 @@ import javax.swing.SwingUtilities;
 public class Application {
 
 	public static void main(String[] args) {
-		Server server = new Server();
 		ServerLogDisplay logDisplay = new ServerLogDisplay();
 		SwingUtilities.invokeLater(logDisplay);
+
+		Server server = new Server();
 		server.start();
 	}
 }
--- GeometryServer/src/my/games/geometry/game/ServerLogDisplay.java
+++ GeometryServer/src/my/games/geometry/game/ServerLogDisplay.java
@@ -1,10 +1,21 @@
 package my.games.geometry.game;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextPane;
 
 public class ServerLogDisplay implements Runnable {
+
+	private List<JLabel> mLabelList;
+	private List<JTextPane> mTextList;
+
+	public ServerLogDisplay() {
+		mLabelList = new ArrayList<JLabel>();
+		mTextList = new ArrayList<JTextPane>();
+	}
 
 	@Override
 	public void run() {
@@ -12,26 +23,27 @@ public class ServerLogDisplay implements Runnable {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(500, 500);
 		frame.setResizable(false);
-		JLabel[] mLabel = new JLabel[8];
-		final JTextPane[] mText = new JTextPane[8];
-		for (int i = 0; i < 8; i++) {
-			mText[i] = new JTextPane();
-			mText[i].setBounds(0, (i * 50) + 25, 200, 20);
-			mText[i].setText("0");
-			frame.add(mText[i]);
 
-			mLabel[i] = new JLabel();
-			mLabel[i].setBounds(0, (i * 50), 200, 20);
-			frame.add(mLabel[i]);
+		for (int i = 0; i < 8; i++) {
+			JTextPane anotherTextField = new JTextPane();
+			anotherTextField.setBounds(0, (i * 50) + 25, 200, 20);
+			anotherTextField.setText("0");
+			mTextList.add(i, anotherTextField);
+			frame.add(anotherTextField);
+
+			JLabel anotherLabel = new JLabel();
+			anotherLabel.setBounds(0, (i * 50), 200, 20);
+			mLabelList.add(i, anotherLabel);
+			frame.add(anotherLabel);
 		}
-		mLabel[0].setText("gameObjectsMap");
-		mLabel[1].setText("drawableObjectList");
-		mLabel[2].setText("updatableObjectList");
-		mLabel[3].setText("collidableObjectList");
-		mLabel[4].setText("shootersList");
-		mLabel[5].setText("hz");
-		mLabel[6].setText("hz");
-		mLabel[7].setText("hz");
+		mLabelList.get(0).setText("gameObjectsMap");
+		mLabelList.get(1).setText("drawableObjectList");
+		mLabelList.get(2).setText("updatableObjectList");
+		mLabelList.get(3).setText("collidableObjectList");
+		mLabelList.get(4).setText("shootersList");
+		mLabelList.get(5).setText("hz");
+		mLabelList.get(6).setText("hz");
+		mLabelList.get(7).setText("hz");
 
 		// mText[0].setText(server.getWorld().get gameObjectsMap.getSize());
 		// mText[0].setText(Integer.toString(server.getWorld().getDrawableObjectList().size()));
