0274c9bccedbdb3a9b8e3b56d38aefb7a6c9805b
qwerty367 <Daenisvolkov@gmail.com>
2016-09-27 23:39:33 +0300

    renamed classes

--- GeometryServer/src/my/games/geometry/game/Application.java
+++ GeometryServer/src/my/games/geometry/game/Application.java
@@ -2,6 +2,8 @@ package my.games.geometry.game;
 
 import javax.swing.SwingUtilities;
 
+import my.games.geometry.ui.ServerLogDisplay;
+
 public class Application {
 
 	public static void main(String[] args) {
