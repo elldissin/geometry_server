7a2f4359567fd3dc3c8861bd2a3fd82b0e7478c6
elldissin <elldissin@gmail.com>
2017-08-26 22:51:24 +0300

    renaming class

--- GeometryServer/src/my/games/geometry/game/Application.java
+++ GeometryServer/src/my/games/geometry/game/Application.java
@@ -3,12 +3,12 @@ package my.games.geometry.game;
 import javax.swing.SwingUtilities;
 
 import my.games.geometry.game.engine.Server;
-import my.games.geometry.ui.ServerLogDisplay;
+import my.games.geometry.ui.ServerStatusWindow;;
 
 public class Application {
 
 	public static void main(String[] args) {
-		ServerLogDisplay logDisplay = new ServerLogDisplay();
+		ServerStatusWindow logDisplay = new ServerStatusWindow();
 		SwingUtilities.invokeLater(logDisplay);
 
 		Server server = new Server();
